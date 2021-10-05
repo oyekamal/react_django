d36104f1625a5f0c35eb11985b3e3b7892c9da70
kamalrapi <muhammad.kamal@rapidev.ae>
2021-10-05 12:04:34 +0500

    api done

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+
+router = DefaultRouter()
+router.register(r'question', views.QuestionViewSet)
+router.register(r'choice', views.ChoiceViewSet)
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', include(router.urls)),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
-
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class ChoiceViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
